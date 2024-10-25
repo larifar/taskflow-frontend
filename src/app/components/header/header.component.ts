0ed13d25d0b9fcac64f6ab6f7d3d84831e1eb778
Larissa Faria <lari.f4ria@gmail.com>
2024-10-25 10:57:27 -0300

    created header and icon components

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { IconComponent } from "../icon/icon.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [IconComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
