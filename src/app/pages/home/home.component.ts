f1020d3f69121ad8920f922d4d356b5900c96327
Larissa Faria <lari.f4ria@gmail.com>
2024-11-06 02:14:31 -0300

    created logic to grid calendar test

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { WeekCalendarComponent } from '../../components/week-calendar/week-calendar.component';
+import { TestCalendarComponent } from "../../components/test-calendar/test-calendar.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ WeekCalendarComponent],
+  imports: [WeekCalendarComponent, TestCalendarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
